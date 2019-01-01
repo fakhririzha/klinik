ac2e2123c92b2636a34f08b071eda18d7b2b24b9
dinulimann <dinulimankappa@gmail.com>
2019-01-01 00:10:33 -0800

    Staff Kesehatan

---
+++ application/views/staff_kesehatan/content/form_pk.php
@@ -0,0 +1,80 @@
+<section id="content">
+    <!--start container-->
+    <div class="container">
+        <!--card stats start-->
+        <div id="card-stats">
+	<div class="row">
+	<div class="col s2"></div>
+    <form class="col s8">
+	<div class="card-panel white">
+	<br>
+      <div class="row">
+			<center><h3>Pemeriksaan Kesehatan</h3></center>
+        <div class="input-field col s12">
+          <input id="pd_nama" type="text" class="validate">
+          <label for="password">ID Pasien</label>
+        </div>
+      </div>
+	  <div class="row">
+        <div class="input-field col s12">
+          <input id="pd_jenis_kelamin" type="text" class="validate">
+          <label for="password">Nama Pasien</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="text" class="validate">
+          <label for="password">Tempat Lahir</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="date" class="validate">
+          <label for="password">Tanggal Lahir</label>
+        </div>
+      </div>
+	  <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="text" class="validate">
+          <label for="password">Alamat</label>
+        </div>
+      </div>
+	  <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="number" class="validate">
+          <label for="password">Nomor Darurat</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="number" class="validate">
+          <label for="email">Nomor Wali</label>
+        </div>
+      </div>
+	  <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="text" class="validate">
+          <label for="password">Relasi dengan Wali</label>
+        </div>
+      </div>
+	  <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">Fasilitas Kesehatan</label>
+        </div>
+      </div>
+	  <br>
+	  <div class="col s8"></div>
+	  <div class="col s4">
+	  <button class="btn waves-effect blue" type="submit" name="action">Submit
+		<i class="material-icons right">send</i>
+	 </button>
+	 </div>
+  <br>
+  <br>
+  </div>
+    </form>
+	<div class="col s2"></div>
+  </div>
+    <!--end container-->
+</section>
