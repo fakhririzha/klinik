e2acb5e7774ba9e16a4e089546e4b9c90c62efac
dinulimann <dinulimankappa@gmail.com>
2019-01-01 01:50:54 -0800

    Peralatan dan Obat

---
+++ application/views/staff_aset/content/obat.php
@@ -0,0 +1,54 @@
+<div class="row"> 
+	<div class="col s2">
+	</div>
+	<div class="col s8">
+	<h3>Daftar Obats</h3>
+	<br><br>
+	<button class="btn waves-effect blue" type="submit" name="action">Tambah data
+		<i class="material-icons right">add</i>
+	 </button>
+	<br><br>
+ <table class="striped centered">
+        <thead>
+          <tr>
+              <th>ID</th>
+              <th>Nama Obat</th>
+			  <th>Jenis Obat</th>
+              <th>Harga Beli Obat</th>
+			  <th>Harga Jual Obat</th>
+			  <th>Kuantitas Obat</th>
+			  <th>Aksi</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Alvin</td>
+            <td>Eclair</td>
+			<td>Tablet</td>
+			<td>$0.87</td>
+            <td>$0.87</td>
+			<td>87</td>
+			<th><i class="material-icons">edit</i>&nbsp;&nbsp;<i class="material-icons">delete</i></th>
+          </tr>
+          <tr>
+            <td>Alan</td>
+            <td>Jellybean</td>
+			<td>Pil</td>
+            <td>$3.76</td>
+			<td>$3.76</td>
+			<td>87</td>
+			<th><i class="material-icons">edit</i>&nbsp;&nbsp;<i class="material-icons">delete</i></th>
+          </tr>
+          <tr>
+            <td>Jonathan</td>
+            <td>Lollipop</td>
+			<td>Kapsul</td>
+            <td>$7.00</td>
+			<td>$7.00</td>
+			<td>87</td>
+			<th><i class="material-icons">edit</i>&nbsp;&nbsp;<i class="material-icons">delete</i></th>
+          </tr>
+        </tbody>
+ </table>
+	</div>
+</div>
---
+++ application/views/staff_aset/content/peralatan.php
@@ -0,0 +1,46 @@
+<div class="row"> 
+	<div class="col s2">
+	</div>
+	<div class="col s8">
+	<h3>Daftar Peralatan</h3>
+	<br><br>
+	<button class="btn waves-effect blue" type="submit" name="action">Tambah data
+		<i class="material-icons right">add</i>
+	 </button>
+	<br><br>
+ <table class="striped centered">
+        <thead>
+          <tr>
+              <th>ID</th>
+              <th>Nama Item</th>
+              <th>Harga Item</th>
+			  <th>Kuantitas Item</th>
+			  <th>Aksi</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Alvin</td>
+            <td>Eclair</td>
+            <td>$0.87</td>
+			<td>87</td>
+			<th><i class="material-icons">edit</i>&nbsp;&nbsp;<i class="material-icons">delete</i></th>
+          </tr>
+          <tr>
+            <td>Alan</td>
+            <td>Jellybean</td>
+            <td>$3.76</td>
+			<td>87</td>
+			<th><i class="material-icons">edit</i>&nbsp;&nbsp;<i class="material-icons">delete</i></th>
+          </tr>
+          <tr>
+            <td>Jonathan</td>
+            <td>Lollipop</td>
+            <td>$7.00</td>
+			<td>87</td>
+			<th><i class="material-icons">edit</i>&nbsp;&nbsp;<i class="material-icons">delete</i></th>
+          </tr>
+        </tbody>
+ </table>
+	</div>
+</div>
