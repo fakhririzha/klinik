f1b1be3969a3eafdaccb48a9d01baca5ffe9478a
dinulimann <dinulimankappa@gmail.com>
2018-12-31 23:27:57 -0800

    Resepsionis

---
+++ application/views/resepsionis/content/ambil_no_antri.php
@@ -0,0 +1,24 @@
+</br></br>
+<div class="row">
+	<div class="col s2">
+	</div>
+	<div class="col s4">
+<br><br><br>
+		<h5>No.Antri Sekarang :</h5>
+<br><br><br><br><br><br>
+		<h5>Ambil No. Antri&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</h5>
+	</div>
+	<div class="col s2">
+		<div class="card-panel white">
+		<h1>&nbsp;56</h1>
+		</div>
+		<div class="card-panel white">
+		<h1>&nbsp;98</h1>
+		</div></div>
+		<br><br><br><br><br><br><br><br><br><br><br><br>
+		<div class="col s3">
+		<button class="btn waves-effect blue" type="submit" name="action">Ambil
+		<i class="material-icons right">send</i>
+		</div>
+	<div class="col s1">
+	</div>
---
+++ application/views/resepsionis/content/cetak_kartu_pasien.php
@@ -0,0 +1,44 @@
+<br>
+<div class="row">
+<div class= "col s1">
+</div>
+<div class="card medium">
+<div class= "col s3">
+	<img class="responsive-img"  src="<?= base_url('assets/images/online1.jpg') ?>">
+</div>
+<div class="col s6">
+	<table>
+        <thead></thead>
+
+        <tbody>
+          <tr>
+            <td>ID Pasien</td>
+            <td>11102930129</td>
+          </tr>
+          <tr>
+            <td>Nama Lengkap</td>
+            <td>Bambang Syahpurta</td>
+          </tr>
+          <tr>
+            <td>Jenis Kelamin</td>
+            <td>Laki-laki</td>
+          </tr>
+		  <tr>
+            <td>Tempat Lahir</td>
+            <td>Medan</td>
+          </tr>
+		  <tr>
+            <td>Tanggal Lahir</td>
+            <td>12 Januari 2000</td>
+          </tr>
+		  <tr>
+            <td>Fasilitas Kesehatan</td>
+            <td>Klinik Harapan Bunda</td>
+          </tr>
+        </tbody>
+      </table>
+</div>
+</div>
+<div class= "col s2">
+</div>
+</div>
