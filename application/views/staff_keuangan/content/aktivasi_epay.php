a9ee016d7b9144368f542820f3ea521eda50a2f7
dinulimann <dinulimankappa@gmail.com>
2019-01-04 23:11:26 -0800

    Aktivasi e-pay dan top-up

---
+++ application/views/staff_keuangan/content/aktivasi_epay.php
@@ -0,0 +1,115 @@
+<div class="row"> 
+	<div class="col s2">
+	</div>
+	<div class="col s8">
+	<h3>Aktivasi E-Pay</h3>
+	<br><br>
+	
+	 </button>
+	<br><br>
+ <table class="striped centered">
+        <thead>
+          <tr>
+              <th>ID</th>
+              <th>Nama Pasien</th>
+			  <th>Aktivasi</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Alvin</td>
+            <td>Eclair</td>
+			<th> <!-- Modal Trigger -->
+  <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Aktivasi E-pay</a>
+
+  <!-- Modal Structure -->
+  <div id="modal1" class="modal">
+    <div class="modal-content">
+      <h4><center>Aktivasi E-pay</center></h4>
+		<table >
+			<thead>
+			</thead>
+			<tbody>
+				<tr>
+					<td>ID Pasien :</td>
+					<td>19283938</td>
+				</tr>
+				<tr>
+					<td>Nama Pasien :</td>
+					<td>Kappa Rampa</td>
+				</tr>
+			</tbody>
+		  </table>
+	  </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Cancel</a>
+	  <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+    </div>
+  </div></th>
+          </tr>
+          <tr>
+            <td>Alan</td>
+            <td>Jellybean</td>
+			<th> <!-- Modal Trigger -->
+  <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Aktivasi E-pay</a>
+
+  <!-- Modal Structure -->
+  <div id="modal1" class="modal">
+    <div class="modal-content">
+      <h4><center>Aktivasi E-pay</center></h4>
+		<table >
+			<thead>
+			</thead>
+			<tbody>
+				<tr>
+					<td>ID Pasien :</td>
+					<td>19283938</td>
+				</tr>
+				<tr>
+					<td>Nama Pasien :</td>
+					<td>Kappa Rampa</td>
+				</tr>
+			</tbody>
+		  </table>
+	  </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Cancel</a>
+	  <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+    </div>
+  </div></th>
+          </tr>
+          <tr>
+            <td>Jonathan</td>
+            <td>Lollipop</td>
+			<th> <!-- Modal Trigger -->
+  <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Aktivasi E-pay</a>
+
+  <!-- Modal Structure -->
+  <div id="modal1" class="modal">
+    <div class="modal-content">
+      <h4><center>Aktivasi E-pay</center></h4>
+		<table >
+			<thead>
+			</thead>
+			<tbody>
+				<tr>
+					<td>ID Pasien :</td>
+					<td>19283938</td>
+				</tr>
+				<tr>
+					<td>Nama Pasien :</td>
+					<td>Kappa Rampa</td>
+				</tr>
+			</tbody>
+		  </table>
+	  </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Cancel</a>
+	  <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+    </div>
+  </div></th>
+          </tr>
+        </tbody>
+ </table>
+	</div>
+</div>
